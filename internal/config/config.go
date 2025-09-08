b5185c2ed370a6050be8a5c8058d15d83313160d
Pradeep Kumar <pradeepitm12@gmail.com>
2025-09-08 16:45:18 +0530

    enables hosted signoz-mcp-server
    this adds environment variables to enalbe local and cloud hosted
    mcp-server integration, currently only working with cursor.
    DEPLOYMENT_MODE :<cloud/local>, MCP_SERVER_PORT
    
    Currently it uses API Key, need to change it to OAuth/or similar

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,33 @@
 package config
 
 import (
+	"fmt"
 	"os"
 )
 
 type Config struct {
-	URL      string
-	APIKey   string
-	LogLevel string
+	URL            string
+	APIKey         string
+	LogLevel       string
+	DeploymentMode string
+	Port           string
 }
 
 const (
-	SignozURL    = "SIGNOZ_URL"
-	SignozApiKey = "SIGNOZ_API_KEY"
-	LogLevel     = "LOG_LEVEL"
+	SignozURL      = "SIGNOZ_URL"
+	SignozApiKey   = "SIGNOZ_API_KEY"
+	LogLevel       = "LOG_LEVEL"
+	DeploymentMode = "DEPLOYMENT_MODE"
+	MCPPort        = "MCP_SERVER_PORT"
 )
 
 func LoadConfig() (*Config, error) {
 	return &Config{
-		URL:      getEnv(SignozURL, ""),
-		APIKey:   getEnv(SignozApiKey, ""),
-		LogLevel: getEnv(LogLevel, "info"),
+		URL:            getEnv(SignozURL, ""),
+		APIKey:         getEnv(SignozApiKey, ""),
+		LogLevel:       getEnv(LogLevel, "info"),
+		DeploymentMode: getEnv(DeploymentMode, "local"),
+		Port:           getEnv(MCPPort, "8000"),
 	}, nil
 }
 
@@ -29,4 +36,20 @@ func getEnv(key, defaultValue string) string {
 		return value
 	}
 	return defaultValue
+}
+
+func (c *Config) ValidateConfig() error {
+	if c.URL == "" {
+		return fmt.Errorf("SIGNOZ_URL is required")
+	}
+	if c.APIKey == "" {
+		return fmt.Errorf("SIGNOZ_API_KEY is required")
+	}
+
+	if c.DeploymentMode == "cloud" {
+		if c.Port == "" {
+			return fmt.Errorf("MCP_SERVER_PORT is required for cloud deployment")
+		}
+	}
+	return nil
 }
